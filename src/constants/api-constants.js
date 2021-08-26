1c897c31700c1a8c8ad4eb8eeaa88e636f24169a
Rathish Kumar <rathishudhay@gmail.com>
2021-08-27 00:19:16 +0530

    updated server url

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,4 +1,4 @@
 // export const apiRootUrl = "http://localhost:3001"
-export const apiRootUrl = "http://ec2-13-233-255-61.ap-south-1.compute.amazonaws.com:3000/api/v1"
+export const apiRootUrl = "https://d3eza0b5pcyi2u.cloudfront.net/api/v1"
 export const onUserConnectUrl = apiRootUrl + "/user/newConnect"
 export const addContactUrl = apiRootUrl + "/user/addContact"
