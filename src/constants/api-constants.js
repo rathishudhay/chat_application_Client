6db8ee012f60923b51f1a923ecf1d3b6a4f5deea
Rathish Kumar <rathishudhay@gmail.com>
2021-08-31 23:40:25 +0530

    prod

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,4 +1,4 @@
 // export const apiRootUrl = "http://localhost:3001"
-export const apiRootUrl = "https://d3eza0b5pcyi2u.cloudfront.net/api/v1"
+export const apiRootUrl = "https://chat-api.rathish.in/api/v1"
 export const onUserConnectUrl = apiRootUrl + "/user/newConnect"
 export const addContactUrl = apiRootUrl + "/user/addContact"
